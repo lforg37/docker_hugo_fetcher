fd90728d3204afcfeae54a1907d3d34dac41f1a7
Luc Forget <luc.forget@insa-lyon.fr>
2017-08-12 16:00:02 +0200

    Debug

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM alpine:latest
 
-RUN apk add --no-cache hugo py-pygments git;\
+RUN apk add --no-cache hugo openssh-client py-pygments git;\
     mkdir build scratch
 
-COPY scripts/entry.sh entry.sh
+COPY scripts/entry.sh /entry.sh
 
-ENTRYPOINT entry.sh
+ENTRYPOINT ["/entry.sh"]
--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
-if test "$#" -ne 2
+if test "$#" -ne 1
 then
+	echo $1
+	echo $#
 	echo "Usage : $0 git-src"
-	exit -1
+	exit 125
 fi
 
 GIT_REPO=$1
-
+echo "Now trying to clone git repo $1"
 cd scratch
 rm -r *
 git clone --recursive --depth 1 $GIT_REPO src
 cd src
+echo "Building the website"
 hugo --cleanDestinationDir -d /build/
