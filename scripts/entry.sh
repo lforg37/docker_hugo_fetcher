980a0defde3a158ca2e6f1873130e02d27e069a4
Luc Forget <luc.forget@insa-lyon.fr>
2017-08-12 18:32:59 +0200

    Modifs

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
-if test "$#" -ne 1
+if test "$#" -ne 2
 then
 	echo $1
 	echo $#
-	echo "Usage : $0 git-src"
+	echo "Usage : $0 git-src domain"
 	exit 125
 fi
 
 GIT_REPO=$1
+DOMAIN=$2
 echo "Now trying to clone git repo $1"
 cd scratch
 git clone --recursive --depth 1 $GIT_REPO src
 cd src
 echo "Building the website"
-hugo --cleanDestinationDir 
-mv public/* /build/
+hugo -b $DOMAIN --cleanDestinationDir --destination /build 
